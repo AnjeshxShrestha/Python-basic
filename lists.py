c8e6b2c38e8257ae782db42650bfe03e67cc13c9
AnjeshxShrestha <anjeshshrestha039@gmail.com>
2025-05-21 21:27:54 +0545

    Create lists.py

---
+++ lists.py
@@ -0,0 +1,9 @@
+fruits = ["apple", "banana", "cherry"]
+
+print("First fruit:", fruits[0])
+fruits.append("orange")
+print("After adding:", fruits)
+
+print("All fruits:")
+for fruit in fruits:
+    print(fruit)
